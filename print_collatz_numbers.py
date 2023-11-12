82b69ead991ba8ee831437b3de2211afe82f7736
Berk <b______@gmail.com>
2023-11-12 18:49:41 -0500

    Finished print_collatz_numbers.py

--- print_collatz_numbers.py
+++ print_collatz_numbers.py
@@ -11,9 +11,13 @@ except:
     print("You did not provide a whole number")
     exit()
 
+print(f"You provided {number} as input")
 result = compute_numbers(number)
+index = 0
 for n in result:
-    print(n)
+    print(f"Step {index:4} {n:10}")
+    index += 1
+print(f"Total number of steps till we reached 1: {len(result)-1} steps")
 
 
 
