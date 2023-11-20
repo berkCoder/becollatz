f262c0de0f136ed5cd5f5c7da68658431adc4ee5
Berk <b______@gmail.com>
2023-11-19 19:08:44 -0500

    can provide range of numbers in command line

--- print_collatz_numbers.py
+++ print_collatz_numbers.py
@@ -16,41 +16,48 @@ def convert_to_integer(str_value: str) -> int:
         exit(1)
 
 
-arguments = []
-i = 1
-while i < len(sys.argv):
-    arg = sys.argv[i]
-    if "-" in arg:
-        first, second = arg.split("-")
-        arguments.append((convert_to_integer(first), convert_to_integer(second)))
-    else:
-        arguments.append(convert_to_integer(arg))
-    i += 1
+def print_collatz_numbers(number: int):
+    print(f"You provided {number} as input")
+    result = compute_numbers(number)
+    index = 0
+    for n in result:
+        print(f"Step {index:4} {n:10}")
+        index += 1
+    print(f"Total number of steps till we reached 1: {len(result) - 1} steps")
 
-print(arguments)
+
+
+def get_arguments():
+    arguments = []
+    i = 1
+    while i < len(sys.argv):
+        arg = sys.argv[i]
+        if "-" in arg:
+            first, second = arg.split("-")
+            first = convert_to_integer(first)
+            second = convert_to_integer(second)
+            if first > second:
+                print(f"{first} is greater than {second} in {arg}")
+                exit()
+            arguments.append((first, second))
+        else:
+            arguments.append(convert_to_integer(arg))
+        i += 1
+    return arguments
+
+
+args = get_arguments()
+for arg in args:
+    if type(arg) == tuple:  # if isinstance(arg, tuple):
+        arg1, arg2 = arg
+        print(arg1, arg2)
+        for i in range(arg1, arg2 + 1):
+            print_collatz_numbers(i)
+    else:
+        print_collatz_numbers(arg)
+
 
 sys.exit(1)
 
 
 
-
-
-
-
-
-
-
-
-try:
-    number = int(sys.argv[1])
-except:
-    print("You did not provide a whole number")
-    exit()
-
-print(f"You provided {number} as input")
-result = compute_numbers(number)
-index = 0
-for n in result:
-    print(f"Step {index:4} {n:10}")
-    index += 1
-print(f"Total number of steps till we reached 1: {len(result)-1} steps")
